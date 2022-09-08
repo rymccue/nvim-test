215b1ae226040e0d59d2f138036dddc0a4ab016d
Ryan McCue <ryan@msys.ca>
2022-09-08 10:52:59 -0600

    Update pytest.lua

--- lua/nvim-test/runners/pytest.lua
+++ lua/nvim-test/runners/pytest.lua
@@ -2,7 +2,7 @@ local Runner = require "nvim-test.runner"
 
 local pytest = Runner:init({
   command = { "docker-compose" },
-  args = { "exec", "web", "python", "manage.py", "test", "--no-input", "--parallel", "--keepdb" },
+  args = { "exec", "web", "python", "manage.py", "test", "--parallel", "--no-input" },
   file_pattern = "\\v(test_[^.]+|[^.]+_test|tests)\\.py$",
   find_files = { "test_{name}.py", "{name}_test.py", "tests.py" },
 }, {
